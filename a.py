c4d3c96f01e929ae873755ee4d3233578b2876c1
ManisaiReddy <manisaisaduvala21@gmail.com>
2025-02-06 14:26:49 -0800

    added question in a.py

--- a.py
+++ a.py
@@ -4,3 +4,4 @@ def greetCandidates():
 greetCandidates()
 # write  a code for finding prime numbers from 1 to 10
 # write a fibannoci series program
+# write a program on palindrome numbers
--- add.py
+++ add.py
@@ -1,3 +1,4 @@
 def sum(num1,num2):
     return num1+num2
 sum(3,2)
+# return the addition pass to variable
